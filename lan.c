01d2dc8a82ca2c56f264bdf125136e5188a2b215
xianjie-alone <zan__lei@163.com>
2020-12-23 20:28:27 +0800

    add .c file

---
+++ lan.c
@@ -0,0 +1,12 @@
+/*
+ * Author: ZanLei
+ * Date: 2020-12-23
+ * Description: some commit
+ * Contact information: l.zan@hotmail.com
+*/
+
+#include <stdio.h>
+int main(){
+    printf("%s","hi");
+    return 0;
+}
